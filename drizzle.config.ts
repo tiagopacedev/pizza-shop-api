17d0e071308cd0a3a8eae374e7bd27db04ab7b82
Tiago Pace <tiagopace.dev@gmail.com>
2024-09-30 18:50:38 -0300

    feat(db): add migration setup for Drizzle ORM

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema/index.ts',
   out: './drizzle',
+  dbCredentials: {
+    url: 'postgres://docker:docker@localhost:5432/pizza_shop',
+  },
 })
