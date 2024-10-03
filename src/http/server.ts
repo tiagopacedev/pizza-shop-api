3992ef3351c38ab824fc7a2be1bc098c8aaa5fba
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-03 18:22:38 -0300

    feat(server): set up JWT authentication

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,21 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    })
+  )
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
