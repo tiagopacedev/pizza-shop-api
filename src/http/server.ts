7f8fc9196840de4474dcd2939679966fcfe7e643
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-03 18:22:22 -0300

    feat(routes): add route for sending authentication link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,8 @@
 import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant)
+const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
