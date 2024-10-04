3ab0fbe1ca0ec92b8bb52e4a253fe8b8bfa597cf
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-04 12:57:59 -0300

    feat(routes): add logged-in user profile route

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -31,5 +31,18 @@ export const auth = new Elysia()
       signOut: async () => {
         auth.remove()
       },
+
+      getCurrentUser: async () => {
+        const payload = await jwt.verify(auth.value)
+
+        if (!payload) {
+          throw new Error('Unauthorized.')
+        }
+
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
+      },
     }
   })
